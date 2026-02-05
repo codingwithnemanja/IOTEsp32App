61bbb06c958da3692dd0103d7b6ca647b989ceda
Nemanja Tosic <ntosic693@gmail.com>
2026-02-05 19:30:27 +0100

    Added connection to host.

--- main.go
+++ main.go
@@ -4,33 +4,67 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
 	"time"
 
 	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
 func main() {
+	// Statički link ka bazi (Docker DNS koristi "db")
 	connStr := "postgres://nemanja:mojasifra@db:5432/iot_db?sslmode=disable"
 
-	db, err := sql.Open("postgres", connStr)
+	var err error
+	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
+	// Čekanje da se baza podigne
 	for i := 0; i < 5; i++ {
-		err = db.Ping()
-		if err == nil {
+		if err = db.Ping(); err == nil {
 			break
 		}
+		fmt.Println("Baza nije spremna, čekam...")
 		time.Sleep(2 * time.Second)
 	}
 
+	// Kreiranje tabele za merenja
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS merenja (
+		id SERIAL PRIMARY KEY, 
+		vrednost TEXT, 
+		vreme TIMESTAMP DEFAULT NOW()
+	)`)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("USPEH: Tabela je spremna!")
 
-	fmt.Println("USPEH: Povezan na PostgreSQL bazu!")
+	// REST API RUTE
+	http.HandleFunc("/podaci", primiPodatke) // ESP32 šalje ovde: POST /podaci?temp=25
+	http.HandleFunc("/led", vratiBoju)       // ESP32 čita odavde: GET /led
 
-	select {}
+	fmt.Println("Server pokrenut na portu 8080...")
+
+	// ListenAndServe drži program budnim zauvek (nema više deadlock-a)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func primiPodatke(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		temp := r.URL.Query().Get("temp")
+		_, err := db.Exec("INSERT INTO merenja (vrednost) VALUES ($1)", temp)
+		if err != nil {
+			http.Error(w, "Greška u bazi", 500)
+			return
+		}
+		fmt.Fprintf(w, "Uspešno upisano: %s", temp)
+	}
+}
+
+func vratiBoju(w http.ResponseWriter, r *http.Request) {
+	// Ovde kasnije možeš čitati iz baze, sada šaljemo statički
+	fmt.Fprintf(w, "CRVENA")
 }
