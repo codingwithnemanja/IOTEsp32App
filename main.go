50c889bfc81ad93514e47853ee384d590da736e9
Nemanja Tosic <ntosic693@gmail.com>
2026-02-05 19:13:55 +0100

    Added essential files for launch.

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/lib/pq"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
 func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	connStr := "postgres://nemanja:mojasifra@db:5432/iot_db?sslmode=disable"
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer db.Close()
+
+	for i := 0; i < 5; i++ {
+		err = db.Ping()
+		if err == nil {
+			break
+		}
+		time.Sleep(2 * time.Second)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("USPEH: Povezan na PostgreSQL bazu!")
+
+	select {}
 }
